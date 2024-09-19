10f004c4182ccbe7b4007fa9a777a76e9d9849d7
DuyHung03 <duyhung180903@gmail.com>
2024-09-19 08:52:23 +0700

    update

--- src/main/java/com/duyhung/bookstoreapi/service/EmailService.java
+++ src/main/java/com/duyhung/bookstoreapi/service/EmailService.java
@@ -83,8 +83,8 @@ public class EmailService {
         log.info("Google Authorization Code Flow initialized.");
 
         LocalServerReceiver receiver = new LocalServerReceiver.Builder()
-                .setHost("book-haven-api-1.onrender.com")
-                .setPort(8080)
+                .setHost("book-haven-api.onrender.com")
+                .setCallbackPath("/Callback")
                 .build();
         log.info("Local Server Receiver initialized on port 8888.");
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
